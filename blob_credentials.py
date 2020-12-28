5eb1d1338691bb199deeba7057e1f474219f7625
Sarah Jallot <sarah.jallot@hec.edu>
2020-12-28 08:41:57 +0100

    remove old version

--- blob_credentials.py
+++
@@ -1,4 +0,0 @@
-facts_sas_token = "se=2020-10-15&sp=rl&spr=https&sv=2018-11-09&sr=c&sig=A9/0jGPBe/i1b8qHkrplGg17vokAu%2Bih1izG0tFSl4U%3D"
-facts_container = "eleven-2020-facts"
-workspace_sas_token = "se=2020-10-15&sp=rwdl&spr=https&sv=2018-11-09&sr=c&sig=GzPBS4pTVy8/NviycLhzernm0KFoQPembj98sBnhf44%3D"
-workspace_container = "eleven-2020-workspace-group3"
